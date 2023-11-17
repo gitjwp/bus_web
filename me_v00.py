ac69ceea7e1068b75c9c33b92e0e1787367ce80c
gitjwp <151175066+gitjwp@users.noreply.github.com>
2023-11-18 04:22:31 +0900

    Add files via upload

---
+++ me_v00.py
@@ -0,0 +1,56 @@
+# 제목 출력
+import streamlit as st
+def print_title(title):
+    st.write("### " + title)
+
+# 정류장 단위 출력
+import requests
+def print_station(Id, selection, selected_bus):
+    # URL
+    url = 'https://map.naver.com/p/api/pubtrans/realtime/bus/arrivals?stopId='+Id
+
+    # 1단계 : 요청
+    req = requests.get(url)
+    data = str(req.text)
+
+    # 2단계 : 반응을 리스트로 변환
+    import json
+    data = json.loads(data)
+
+
+    # 3단계 : 자료를 적절하게 표시
+    for bus in data:
+        if selection and not bus["name"] in selected_bus:
+            continue
+    
+        st.write("#### :green[" + bus["longName"] + " (" + bus["direction"] + " 방면)]")
+        if bus["arrival"]["status"] == "RUNNING":
+            for next_bus in bus["arrival"]["buses"]:
+                sec = int(next_bus["remainingTime"])
+                if sec < 300:
+                    text = ":orange[" + str(sec // 60) + "분 " + str(sec % 60) + "초], " + next_bus["remainingStop"] + "역 전"
+                else:
+                    text = ":blue[" + str(sec // 60) + "분 " + str(sec % 60) + "초], " + next_bus["remainingStop"] + "역 전"
+                if next_bus["remainingSeat"] != None:
+                    text += ", 남은 좌석 : " + next_bus["remainingSeat"]
+                st.write(text)
+        else:
+            st.write(":red[운행 안 하는 중]")
+
+def main():
+    from datetime import datetime
+    now = datetime.now()
+    # 학원 가는거
+    if now.hour < 15:
+        print_title("집 > 학원")
+        print_station("115846", True, "112")
+        print_title("학원 > 집")
+        print_station("115065", True, "112")
+    # 집 가는거
+    else:
+        print_title("학원 > 집")
+        print_station("115065", True, "112")
+        print_title("집 > 학원")
+        print_station("115846", True, "112")
+
+main()
---
+++ wm_v00.py
@@ -0,0 +1,58 @@
+# 제목 출력
+import streamlit as st
+def print_title(title):
+    st.write("### " + title)
+
+# 정류장 단위 출력
+import requests
+def print_station(Id, selection, selected_bus):
+    # URL
+    url = 'https://map.naver.com/p/api/pubtrans/realtime/bus/arrivals?stopId='+Id
+
+    # 1단계 : 요청
+    req = requests.get(url)
+    data = str(req.text)
+
+    # 2단계 : 반응을 리스트로 변환
+    import json
+    data = json.loads(data)
+
+
+    # 3단계 : 자료를 적절하게 표시
+    for bus in data:
+        if selection and not bus["name"] in selected_bus:
+            continue
+    
+        st.write("#### :green[" + bus["longName"] + " (" + bus["direction"] + " 방면)]")
+        if bus["arrival"]["status"] == "RUNNING":
+            for next_bus in bus["arrival"]["buses"]:
+                sec = int(next_bus["remainingTime"])
+                if sec < 300:
+                    text = ":orange[" + str(sec // 60) + "분 " + str(sec % 60) + "초], " + next_bus["remainingStop"] + "역 전"
+                else:
+                    text = ":blue[" + str(sec // 60) + "분 " + str(sec % 60) + "초], " + next_bus["remainingStop"] + "역 전"
+                if next_bus["remainingSeat"] != None:
+                    text += ", 남은 좌석 : " + next_bus["remainingSeat"]
+                st.write(text)
+        else:
+            st.write(":red[운행 안 하는 중]")
+
+def main():
+    from datetime import datetime
+    weekday = datetime.today().weekday()
+    # 학원 > 집
+    if weekday == 3 or weekday == 4:
+        print_title("학원에서 집가기")
+        print_station('101480', True, ['5712'])
+        print_title("집에서 학교가기")
+        print_station('101401', True, ['603', '5012', '6617'])
+    # 집 > 학교
+    else:
+        print_title("집에서 학교가기")
+        print_station('101401', True, ['603', '5012', '6617'])
+        print_title("학원에서 집가기")
+        print_station('101480', True, ['5712'])
+
+main()
+
+print_title("후후 조티미가윰")
